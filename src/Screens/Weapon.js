610d648a37bf5dbb294b990381f7eb53c4708bd2
Fakhri <fakhridho95@gmail.com>
2021-03-17 09:42:41 +0700

    fixing bug and update

--- src/Screens/Weapon.js
+++ src/Screens/Weapon.js
@@ -1,49 +1,47 @@
-import React, {Component, useState, useEffect} from 'react';
-import {connect} from 'react-redux';
+import React, { Component, useState, useEffect } from "react";
+import { connect } from "react-redux";
 
-import {getWeapon} from '../Publics/redux/actions/weapon';
+import { getWeapon } from "../Publics/redux/actions/weapon";
 
-function Weapon () {
-  const [weapon, setWeapon] = useState([])
+function Weapon() {
+  const [weapon, setWeapon] = useState([]);
 
   useEffect(() => {
-    const result = this.props.dispatch(getWeapon())
-    setWeapon(result)
-  }, [])
+    const result = this.props.dispatch(getWeapon());
+    setWeapon(result);
+  }, []);
 
-  render() {
-    const list = weapon.weaponList;
-    console.log(list);
-    return (
-      <div>
-        <ul>
-          {list &&
-            list.data.entries.length > 0 &&
-            list.data.entries.map((entry, index) => {
-              return (
-                <li
-                  key={index}
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                  }}
-                >
-                  <img
-                    src={entry.image}
-                    style={{height: 250, width: 250}}
-                    alt={index.toString()}
-                  />
-                  <h3>{entry.name}</h3>
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-    );
-  }
+  const list = weapon.weaponList;
+  console.log(list);
+  return (
+    <div>
+      <ul>
+        {list &&
+          list.data.entries.length > 0 &&
+          list.data.entries.map((entry, index) => {
+            return (
+              <li
+                key={index}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <img
+                  src={entry.image}
+                  style={{ height: 250, width: 250 }}
+                  alt={index.toString()}
+                />
+                <h3>{entry.name}</h3>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     weapon: state.weapon,
   };
