a002d0a74613ab39ec849f927032189079755eaa
Fakhri <fakhridho95@gmail.com>
2021-03-17 09:57:14 +0700

    fix browserlist

--- src/Screens/Weapon.js
+++ src/Screens/Weapon.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 
 import { getWeapon } from "../Publics/redux/actions/weapon";
